63317603af48cf36f09d79972c50910f0bcabc5d
Tom Walsh <hello@tmwlsh.co.uk>
2020-09-24 12:43:46 +0100

    adds staging site

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+         build: {
+         options: {
+	        compress: {
+		        drop_console: false
+		      },
+	        beautify: false,
+	        mangle: true
+	      },
+         	files: {
+         		'js/site.min.js': ['js/site.js']
+         	}
+	      }
+		},
+
+		watch: {
+			options: {
+		        livereload: true,
+		    },
+		    scripts: {
+		        files: ['js/*.js'],
+		        tasks: ['uglify'],
+		        options: {
+		            spawn: false,
+		        },
+		    },
+		    css: {
+			    files: ['css/*.scss'],
+			    tasks: ['sass'],
+			    options: {
+			        spawn: false,
+			    },
+			},
+		},
+
+		sass: {
+		    dist: {
+		        options: {
+		            //style: 'expanded'
+		            style: 'compressed'
+		        },
+		        files: {
+		        'css/main.css': 'css/main.scss'
+		      }
+		    }
+		}
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('deploy', ['sass', 'uglify']);
+
+    grunt.registerTask('default', ['sass', 'uglify','watch']);
+
+};
